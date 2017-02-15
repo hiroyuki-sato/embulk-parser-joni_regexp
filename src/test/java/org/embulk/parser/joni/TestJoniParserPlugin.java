81e738428943fb836df10521764195c89c135c5f
Hiroyuki Sato <hiroysato@gmail.com>
2017-02-15 20:19:11 +0900

    Add tests for required config.

--- src/test/java/org/embulk/parser/joni/TestJoniParserPlugin.java
+++ src/test/java/org/embulk/parser/joni/TestJoniParserPlugin.java
@@ -3,6 +3,7 @@ package org.embulk.parser.joni;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.embulk.EmbulkTestRuntime;
+import org.embulk.config.ConfigException;
 import org.embulk.config.ConfigSource;
 import org.embulk.spi.Exec;
 import org.embulk.spi.util.Newline;
@@ -39,4 +40,24 @@ public class TestJoniParserPlugin
 
     }
 
+    @Test(expected = ConfigException.class)
+    public void checkColumnsRequired()
+    {
+        ConfigSource config = Exec.newConfigSource()
+                .set("format", "(?<name>a*)");
+
+        config.loadConfig(JoniParserPlugin.PluginTask.class);
+    }
+
+    @Test(expected = ConfigException.class)
+    public void checkFormatRequired()
+    {
+        ConfigSource config = Exec.newConfigSource()
+                .set("columns", ImmutableList.of(
+                        ImmutableMap.of(
+                                "name", "name",
+                                "type", "string")));
+
+        config.loadConfig(JoniParserPlugin.PluginTask.class);
+    }
 }

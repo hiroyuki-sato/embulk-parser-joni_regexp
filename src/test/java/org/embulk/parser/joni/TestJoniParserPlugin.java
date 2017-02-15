b2ab0a2898585b0f0294d3fb2c3fa705be3e0252
Hiroyuki Sato <hiroysato@gmail.com>
2017-02-15 21:27:39 +0900

    Add config check tests

--- src/test/java/org/embulk/parser/joni/TestJoniParserPlugin.java
+++ src/test/java/org/embulk/parser/joni/TestJoniParserPlugin.java
@@ -5,18 +5,43 @@ import com.google.common.collect.ImmutableMap;
 import org.embulk.EmbulkTestRuntime;
 import org.embulk.config.ConfigException;
 import org.embulk.config.ConfigSource;
+import org.embulk.config.TaskSource;
 import org.embulk.spi.Exec;
+import org.embulk.spi.FileInput;
+import org.embulk.spi.ParserPlugin;
+import org.embulk.spi.Schema;
+import org.embulk.spi.SchemaConfigException;
+import org.embulk.spi.TestPageBuilderReader;
+import org.embulk.spi.util.InputStreamFileInput;
 import org.embulk.spi.util.Newline;
 import org.joda.time.DateTimeZone;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestJoniParserPlugin
 {
+    private ConfigSource config;
+    private JoniParserPlugin plugin;
+    private TestPageBuilderReader.MockPageOutput output;
+
+    @Before
+    public void createResource()
+    {
+        config = config().set("type", "jsonpath");
+        plugin = new JoniParserPlugin();
+        recreatePageOutput();
+    }
+
     @Rule
     public EmbulkTestRuntime runtime = new EmbulkTestRuntime();
 
@@ -60,4 +85,83 @@ public class TestJoniParserPlugin
 
         config.loadConfig(JoniParserPlugin.PluginTask.class);
     }
+
+    @Test // (expected = SchemaConfigException.class)
+    public void checkNamedCaptureColumnNotFound()
+    {
+        ConfigSource config2 = Exec.newConfigSource()
+                .set("columns", ImmutableList.of(
+                        ImmutableMap.of(
+                                "name", "hoge",
+                                "type", "string")))
+                .set("format", "(?<no_capture_name>a*)");
+
+        config2.loadConfig(JoniParserPlugin.PluginTask.class);
+        try {
+            transaction(config2, fileInput(""));
+        }
+        catch (Throwable t) {
+            assertTrue(t instanceof SchemaConfigException);
+        }
+    }
+
+    @Test(expected = org.joni.exception.SyntaxException.class)
+    public void checkInvalidRegexSyntax()
+            throws Exception
+    {
+        ConfigSource config2 = Exec.newConfigSource()
+                .set("columns", ImmutableList.of(
+                        ImmutableMap.of(
+                                "name", "hoge",
+                                "type", "string")))
+                .set("format", "(?<invalid_regex");
+
+        config2.loadConfig(JoniParserPlugin.PluginTask.class);
+        transaction(config2, fileInput(""));
+    }
+
+
+
+    private void transaction(ConfigSource config, final FileInput input)
+    {
+        plugin.transaction(config, new ParserPlugin.Control()
+        {
+            @Override
+            public void run(TaskSource taskSource, Schema schema)
+            {
+                plugin.run(taskSource, schema, input, output);
+            }
+        });
+    }
+
+    private void recreatePageOutput()
+    {
+        output = new TestPageBuilderReader.MockPageOutput();
+    }
+
+
+    private FileInput fileInput(String... lines)
+            throws Exception
+    {
+        StringBuilder sb = new StringBuilder();
+        for (String line : lines) {
+            sb.append(line).append("\n");
+        }
+
+        ByteArrayInputStream in = new ByteArrayInputStream(sb.toString().getBytes(StandardCharsets.UTF_8));
+        return new InputStreamFileInput(runtime.getBufferAllocator(), provider(in));
+    }
+
+    private InputStreamFileInput.IteratorProvider provider(InputStream... inputStreams)
+            throws IOException
+    {
+        return new InputStreamFileInput.IteratorProvider(
+                ImmutableList.copyOf(inputStreams));
+    }
+
+    private ConfigSource config()
+    {
+        return runtime.getExec().newConfigSource();
+    }
+
 }
